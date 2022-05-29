c8d78eb333629a1f5cbfbffdc63929a17f984935
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-29 15:11:13 -0400

    vue js and tailwind set up

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="w-full bg-slate-800">
+    <nav class="p-4">
+      <h1 class="text-base text-white" >
+          To do App.
+      </h1>
+    </nav>
+  </header>
+</template>
