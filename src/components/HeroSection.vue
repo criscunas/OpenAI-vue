a3f911102c3263cdc1088cff0eb61279c8c143d2
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-30 00:40:02 -0400

    minor style changes

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,8 +3,8 @@
     <h1 class="text-4xl leading-snug tablet:text-5xl tablet:leading-relaxed">
       Book Suggestions with AI Technology
     </h1>
-    <p class="pt-6 text-xl leading-relaxed tablet:pt-8 tablet:text-2xl">
+    <h2 class="pt-6 text-xl leading-relaxed tablet:pt-8 tablet:text-2xl">
       Using OpenAI's powerful API to generate your perfect reading list.
-    </p>
+    </h2>
   </div>
 </template>
