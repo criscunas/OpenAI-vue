dcdf4f58fdc53bdcc0cade4938aae95f5a579833
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-30 16:26:48 -0400

    open ai api set up

--- src/components/HomeForm.vue
+++ src/components/HomeForm.vue
@@ -17,51 +17,60 @@ export default {
     handleFormOne() {
       this.responseData.push(this.response)
       this.response = ""
-        if(this.formOne) {
-          this.formOne = false
-          this.formTwo = true
-      }
+      this.formOne = false
+      this.formTwo = true
     },
     handleFormTwo() {
       this.responseData.push(this.response)
       this.response = ''
-        if(this.formTwo) {
-          this.formTwo = false
-          this.formThree = true
-      }
+      this.formTwo = false
+      this.formThree = true
     },
     submitForm() {
       this.response ? this.responseData.push(this.response) : null
-      this.handler(this.responseData)
+      this.handler()
     }
   }
 }
 </script>
 
 <template>
-  <div v-if="formOne" class="m-4 p-4 max-w-md tablet:m-auto">
+  <div v-if="formOne" class='form-body'>
     <p class="pb-3 tablet:pb-5"> Enter Genre </p>
-    <FormKit type="text" inner-class="border border-gray-400 rounded-lg overflow-hidden focus-within:border-blue-500"
-      input-class="w-full h-8 px-3 border-none text-base text-gray-700 placeholder-gray-400" v-model="response" validation="required" />
-    <div class="pt-3 text-right tablet:pt-5">
+    <FormKit 
+      type="text" 
+      inner-class="form-inner"
+      input-class="form-input" 
+      v-model="response" 
+      validation="required" 
+    />
+    <div class="form-btn">
       <button @click="handleFormOne" class="btn"> Next </button>
     </div>
   </div>
 
-  <div v-if="formTwo" class="m-4 p-4 max-w-md tablet:m-auto">
+  <div v-if="formTwo" class="form-body">
     <p class="pb-3 tablet:pb-5"> Favorite Author </p>
-    <FormKit type="text" inner-class="border border-gray-400 rounded-lg overflow-hidden focus-within:border-blue-500"
-      input-class="w-full h-8 px-3 border-none text-base text-gray-700 placeholder-gray-400" v-model="response" />
-    <div class="pt-3 text-right tablet:pt-5">
+    <FormKit 
+      type="text" 
+      inner-class="form-inner"
+      input-class="form-input" 
+      v-model="response" 
+    />
+    <div class="form-btn">
       <button @click="handleFormTwo" class="btn"> Next </button>
     </div>
   </div>
 
-  <div v-if="formThree" class="m-4 p-4 max-w-md tablet:m-auto">
+  <div v-if="formThree" class="form-body">
     <p class="pb-3 tablet:pb-5"> Favorite Book </p>
-    <FormKit type="text" inner-class="border border-gray-400 rounded-lg overflow-hidden focus-within:border-blue-500"
-      input-class="w-full h-8 px-3 border-none text-base text-gray-700 placeholder-gray-400" v-model="response" />
-    <div class="pt-3 text-right tablet:pt-5">
+    <FormKit 
+      type="text" 
+      inner-class="form-inner"
+      input-class="form-input" 
+      v-model="response" 
+    />
+    <div class="form-btn">
       <button @click="submitForm" class="btn"> Submit </button>
     </div>
   </div>
