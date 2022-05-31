0bf2cd78cb9593e9b2de30041f30ed6c4bf204cc
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-31 15:30:52 -0400

    changed app, just rebuilding engine api from shopify project.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@ import PageHeader from './components/PageHeader.vue'
 import HeroSection from './components/HeroSection.vue'
 import HomeForm from './components/HomeForm.vue'
 import RenderResults from './components/RenderResults.vue'
-import {Configuration, OpenAIApi} from 'openai'
+
 
 export default {
   name: 'App',
   data () {
     return {
-      response : '',
+      response : [],
     }
   },
   components: {
@@ -19,33 +19,18 @@ export default {
     RenderResults
   },
   methods : {
-    async handleSubmit() {
-      
-      const configuration = new Configuration({
-        apiKey: process.env.VUE_APP_api_Key
-      })
-      
-      const openai = new OpenAIApi(configuration)
+    async handleSubmit(value) {
 
-      const questions = [
-        `Give me 3 books in the fiction genre`,
-        'Give me 3 books by Dr.Seuss',
-        'Give me 3 books similar to the The Catcher in the Rye'
-      ]
+      let obj = {
+        prompt: value.prompt, 
+        response: 'test response', 
+      }
 
-      const answer = await Promise.all(
-        questions.map(async(ques) => {
-          const resp = await openai.createCompletion(process.env.VUE_APP_api_Engine, {
-            prompt: ques,
-            temperature: 1,
-            top_p: 1.0,
-            max_tokens: 20,
-          })
-          return resp.data.choices[0].text;
-        })
-      )
-
-      console.log(answer)
+      this.response.push(obj)
+      console.log(this.response)
+    },
+    clearResponses () {
+      this.response = []
     }
   },
   
@@ -55,11 +40,11 @@ export default {
 
 
 <template>
-  <div class='bg-slate-800 h-screen'>
+  <div class='bg-[#333533] h-screen'>
     <PageHeader/>
     <HeroSection/>
     <HomeForm :handler = "handleSubmit"/>
-    <RenderResults :data = "response" />
+    <RenderResults :data = "response" :deleteAll = "clearResponses" />
   </div>
 </template>
 
--- src/components/RenderResults.vue
+++ src/components/RenderResults.vue
@@ -1,15 +1,34 @@
 <script>
+
   export default {
     props: {
-      data: Object
+      data: Object,
+      deleteAll: Function,
     }
   }
 
 </script>
 
 <template>
-  <div class="text-center">
-    <p> {{data || 'awaiting' }} </p>
-    {{data}}
+  <div v-if="data.length == 0" class="text-center">
+    <p class="display-text"> Awaiting Prompt </p>
+  </div>
+
+  
+  <div v-else class="mx-4">
+    <h1 @click="deleteAll" class="display-text"> Clear All</h1>
+    <li v-for="(resp,index) in data" :key="index" class="list-none">
+      <div class="card">
+        <div class="flex flex-row justify-between">
+          <h2 class="card-head" >Prompt</h2>
+          <button @click="deleteHandle(index)">
+            <img class="h-7 w-auto"  src="../../public/images/delete.svg" alt="delete icon" />
+          </button>
+        </div>
+        <p class="card-text"> asdasda</p>
+        <h2 class="card-head mt-6"> AI Response</h2>
+        <p>{{resp.response}}</p>
+      </div>
+    </li>
   </div>
 </template>
