0694af4821b5c154e78ed360cda2f5bf0c14de01
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-31 15:31:30 -0400

    minor style changes

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="p-8 text-white text-center tablet:p-12"> 
     <h1 class="text-4xl leading-snug tablet:text-5xl tablet:leading-relaxed">
-      Book Suggestions with AI Technology
+      GPT-3
     </h1>
     <h2 class="pt-6 text-xl leading-relaxed tablet:pt-8 tablet:text-2xl">
-      Using OpenAI's powerful API to generate your perfect reading list.
+      Test out the various engines by Open AI.
     </h2>
   </div>
 </template>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <header class="w-full bg-[#578FFF]">
+  <header class="w-full bg-[#227c9d]">
     <nav class="p-4 tablet:p-6">
-      <h1 class="font-sans text-xl font-semibold text-white tracking-wider tablet:text-2xl">
-          BookGen
-      </h1>
+      <a href="/" class="font-sans text-xl font-semibold text-white tracking-wider tablet:text-2xl">
+          OpenAI.
+      </a>
     </nav>
   </header>
 </template>
