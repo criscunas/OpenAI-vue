ea53af7b64952c0b698a98b696bd85e83e2c3985
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-06-01 15:58:20 -0400

    changed text

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="p-8 text-white text-center tablet:p-12"> 
-    <h1 class="text-4xl leading-snug tablet:text-5xl tablet:leading-relaxed">
+  <div class="text-white text-center tablet:p-12"> 
+    <h1 class="text-4xl leading-snug pb-4 tablet:text-5xl tablet:leading-relaxed">
       GPT-3
     </h1>
-    <h2 class="pt-6 text-xl leading-relaxed tablet:pt-8 tablet:text-2xl">
+    <h2 class="text-xl leading-relaxed pb-8 tablet:pt-8 tablet:text-2xl">
       Test out the various engines by Open AI.
     </h2>
   </div>
